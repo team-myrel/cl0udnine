8be93841922d64fec3cb30a00122d7e46461a5c4
Rashmi Prakash <rashmiprakash21@hotmail.com>
2019-04-17 14:45:45 -0400

    single change

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import Products from './ProductsReducer'
+import Users from './UserReducer'
 
 const rootReducer = combineReducers({
-  Products
+  Products,
+  Users
 })
 
 export default rootReducer
