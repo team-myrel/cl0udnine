847024f99fb7e3a7725bdd6e2d62f3d30c56f4e4
Rashmi Prakash <rashmiprakash21@hotmail.com>
2019-04-17 16:26:36 -0400

    single Product

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { getProduct} from '../reducers/ProductsReducer'
 
 export class SingleProduct extends Component {
   constructor(props) {
@@ -7,11 +8,36 @@ export class SingleProduct extends Component {
   }
 
   componentDidMount() {
-    
+    this.props.getProduct()
   }
   render() {
+    const product =  this.props.selectedProduct
     return (
-      <div> Hello from singleComponent</div>
+      <div> Hello from singleComponent
+        <img src={product.imgUrl} />
+        <h3>Name: {product.name} </h3>
+        <h4> Description: {product.description} </h4>
+        <h4>Scent: {product.scent} </h4>
+        <h4>Elevation: {product.elevation} </h4>
+        <h5>Quality: {product.quality} </h5>
+        <h3>Price Per Unit: {product.price} </h3>
+        <h5>Benefits: {product.benefits} </h5>
+        <h6>Molecular Composition: {product.molecularComposition} </h6>
+      </div>
     )
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    selectedProduct:state.Products.selectedProduct
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const productId = Number(ownProps.match.params.id)
+  return {
+    getProduct: () => dispatch(getProduct(productId))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct)
