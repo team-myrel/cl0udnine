1351982352040f5a6d6a4401ced566d3796dafff
Lerena Holloway <holloway.lerena@gmail.com>
2019-04-17 21:13:09 -0400

    adds login and signup links for easy access

--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, UserHome, SignupForm} from './components'
-import {me} from './store'
+import {me} from './reducers/UserReducer'
 import AllProducts from './components/AllProducts'
 import SingleProduct from './components/SingleProduct'
 
