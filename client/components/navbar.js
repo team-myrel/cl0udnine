801b277e9f25550e434f6b845083b5a1ab5898bf
Lerena Holloway <holloway.lerena@gmail.com>
2019-04-21 18:08:32 -0400

    sets up jwt

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -23,6 +23,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
+          <Link to="/products">Products</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
         </div>
