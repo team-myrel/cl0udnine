cb7feaa22fa2a3b52d17cef21bfa1239a75abcaf
Lisa Jakubczyk <lisajakubczyk@gmail.com>
2019-04-22 14:40:04 -0400

    TAKING OUT MERGE ERROR IMPORTANT

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../reducers/UserReducer'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { logout } from '../reducers/UserReducer'
 
-<<<<<<< HEAD
 const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div>
-=======
-const Navbar = ({handleClick, isLoggedIn}) => (
   <div id="nav">
->>>>>>> 7d237e5c36bcd7abc4d52e2d14c6a68ac7a7e039
     <nav>
       {isLoggedIn ? (
         <div>
@@ -25,16 +20,16 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           </a>
         </div>
       ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/products">Products</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/cart">
-            <img src="https://image.flaticon.com/icons/svg/2/2772.svg" />
-          </Link>
-        </div>
-      )}
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/products">Products</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/cart">
+              <img src="https://image.flaticon.com/icons/svg/2/2772.svg" />
+            </Link>
+          </div>
+        )}
     </nav>
     <div id="container">
       <div className="title">Welcome to Cloud 9</div>
