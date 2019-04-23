eeb94cfb594c4b21ef3812dbb369e788dade3460
Lisa Jakubczyk <lisajakubczyk@gmail.com>
2019-04-23 14:19:30 -0400

    routing 404 api

--- server/api/productRoutes.js
+++ server/api/productRoutes.js
@@ -1,21 +1,13 @@
 const router = require('express').Router()
-<<<<<<< HEAD
-const {Product} = require('../db/models')
-=======
 const { Product } = require('../db/models')
->>>>>>> stash
-// const isAdmin = require('./isAdmin')
+const isAdmin = require('./isAdmin')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-<<<<<<< HEAD
-    const allProducts = await Product.findAll({order: [['id', 'ASC']]})
-=======
     const allProducts = await Product.findAll(
       { order: [['id', 'ASC']] }
     )
->>>>>>> stash
     res.json(allProducts)
   } catch (err) {
     next(err)
