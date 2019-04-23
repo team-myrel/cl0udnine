83dfd2681ca1288eaeff1d86bf6fea8a19119cdb
Lerena Holloway <holloway.lerena@gmail.com>
2019-04-23 17:03:08 -0400

    adds checkout test

--- client/components/Checkout.spec.js
+++ client/components/Checkout.spec.js
@@ -1,17 +1,20 @@
 import chai, {expect} from 'chai'
 import React from 'react'
-import enzyme, {shoallow} from 'enzyme'
+import enzyme, {shallow} from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import Checkout from './Checkout'
 
-chai.use(sinonChai);
+chai.use(sinonChai)
 
-Enzyme.configure({adapter: new EnzymeAdapter()});
+Enzyme.configure({adapter: new EnzymeAdapter()})
 
 describe('<Checkout>', () => {
-  let checkout = [{
-    
-  }]
+  let checkout = [{item: 'OceanAir', price: 100}]
+
+  let checkoutWrapper = shallow(<Checkout checkout={checkout} total="100" />)
+  it('has a <h3> that renders props.total', () => {
+    expect(checkoutWrapper.find('h3').text()).to.be.equal('100')
+  })
 })
